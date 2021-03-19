ed793ed955544078674433d867b8578cdfe80835
Tanapat Sainak <fallen_things@hotmail.com>
2021-03-19 15:40:50 +0700

    fix: Remove unused vars

--- src/state/farms/fetchFarms.ts
+++ src/state/farms/fetchFarms.ts
@@ -53,7 +53,6 @@ const fetchFarms = async () => {
         tokenDecimals,
         quoteTokenDecimals,
       ] = await multicall(erc20, calls)
-      const gas = BigNumber
 
       // Ratio in % a LP tokens that are in staking, vs the total number in circulation
       const lpTokenRatio = new BigNumber(lpTokenBalanceMC).div(new BigNumber(lpTotalSupply))
