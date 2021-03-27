c6102ebcfee61a53942a73e08a19c4ca56530ae7
Laddawal Kongwong <laddawal.kongwong>
2021-03-27 19:54:18 +0700

    Update style

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -3,18 +3,7 @@ import Container from './Container'
 
 const Page = styled(Container)`
   min-height: calc(100% - 64px);
-  padding-top: 16px;
-  padding-bottom: 16px;
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    padding-top: 24px;
-    padding-bottom: 24px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    padding-top: 32px;
-    padding-bottom: 32px;
-  }
+  padding: 24px;
 `
 
 export default Page
