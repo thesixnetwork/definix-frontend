4e0c24855b2d4f22b0d6a26f1723c6d1e4e7f9d6
Tanapat Sainak <fallen_things@hotmail.com>
2021-05-11 10:56:37 +0700

    fix: Add pool

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -107,6 +107,21 @@ const farms: FarmConfig[] = [
     quoteTokenSymbol: QuoteToken.SIX,
     quoteTokenAdresses: contracts.six,
   },
+  {
+    pid: 7,
+    lpSymbol: 'BNB-BUSD LP',
+    lpAddresses: {
+      97: process.env.REACT_APP_BNB_BUSD_LP_TESTNET,
+      56: process.env.REACT_APP_BNB_BUSD_LP_MAINNET,
+    },
+    tokenSymbol: 'BUSD',
+    tokenAddresses: {
+      97: process.env.REACT_APP_BUSD_ADDRESS_TESTNET,
+      56: process.env.REACT_APP_BUSD_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
   // {
   //   pid: 1,
   //   lpSymbol: 'FINIX-BNB LP',
