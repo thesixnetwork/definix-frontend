5b9cce6a54af0539747d534a7de3fd1da12a462c
Laddawal Kongwong <laddawal.kongwong>
2021-05-12 14:17:26 +0700

    Update wording

--- src/views/TradingChallenge/components/TraderProfileModal.tsx
+++ src/views/TradingChallenge/components/TraderProfileModal.tsx
@@ -134,7 +134,7 @@ const TraderProfileModal = ({ onDismiss = () => null, onSuccessRefresh }) => {
 
         {loadingAPI && loadingContract ? (
           <Button fullWidth variant="primary" onClick={fetchRegister}>
-            Done!
+            Done
           </Button>
         ) : (
           <></>
@@ -150,7 +150,7 @@ const TraderProfileModal = ({ onDismiss = () => null, onSuccessRefresh }) => {
         {!loadingContract ? (
           <Button fullWidth variant="primary">
             <FloatingLogo src={loadingIcon} alt="" width="25" height="25" className="mr-2" />
-            Waiting Confirmation
+            Waiting confirmation
           </Button>
         ) : (
           <></>
