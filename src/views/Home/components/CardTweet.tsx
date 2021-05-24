d9b462b15424144c4f835ec4c1d992fd48a69684
Nutnaree Kitpraserpol <nutnaree@Nutnarees-MacBook-Pro.local>
2021-05-24 22:28:37 +0700

    fix: Create Profile Tweeter

--- src/views/Home/components/CardTweet.tsx
+++ src/views/Home/components/CardTweet.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { Card } from 'uikit-dev'
+import { Card, Text } from 'uikit-dev'
+import { Timeline } from 'react-twitter-widgets'
 
 const Tweet = styled(Card)`
-  padding: 24px;
+  padding: 14px;
   height: 100%;
 `
 
+const BorderStyle = styled(Text)`
+  // border: 1px solid #ececec;
+  border-radius: 6px;
+`
+
 const CardTweet = ({ className = '' }) => {
-  return <Tweet className={className} />
+  return (
+    <>
+      <Tweet className={className}>
+        <BorderStyle>
+          <Timeline
+            dataSource={{
+              sourceType: 'profile',
+              screenName: 'DefinixOfficial',
+            }}
+            options={{
+              chrome: 'noheader, nofooter',
+              height: '400',
+            }}
+          />
+        </BorderStyle>
+      </Tweet>
+    </>
+  )
 }
 
 export default CardTweet
