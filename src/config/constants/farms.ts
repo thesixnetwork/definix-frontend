26fd1e4a4a09d34c3ae034ce358b516b3c29b6df
Tanapat Sainak <fallen_things@hotmail.com>
2021-06-09 15:09:06 +0700

    fix: Add new farm

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -137,6 +137,126 @@ const farms: FarmConfig[] = [
     quoteTokenSymbol: QuoteToken.KUSDT,
     quoteTokenAdresses: contracts.kusdt,
   },
+  {
+    pid: 9,
+    lpSymbol: 'KLAY-KETH LP',
+    lpAddresses: {
+      1001: process.env.REACT_APP_KLAY_KETH_LP_TESTNET,
+      8217: process.env.REACT_APP_KLAY_KETH_LP_MAINNET,
+    },
+    tokenSymbol: 'KETH',
+    tokenAddresses: {
+      1001: process.env.REACT_APP_KETH_ADDRESS_TESTNET,
+      8217: process.env.REACT_APP_KETH_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.KLAY,
+    quoteTokenAdresses: contracts.wklay,
+  },
+  {
+    pid: 10,
+    lpSymbol: 'KLAY-KBTC LP',
+    lpAddresses: {
+      1001: process.env.REACT_APP_KLAY_KBTC_LP_TESTNET,
+      8217: process.env.REACT_APP_KLAY_KBTC_LP_MAINNET,
+    },
+    tokenSymbol: 'KBTC',
+    tokenAddresses: {
+      1001: process.env.REACT_APP_KWBTC_ADDRESS_TESTNET,
+      8217: process.env.REACT_APP_KWBTC_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.KLAY,
+    quoteTokenAdresses: contracts.wklay,
+  },
+  {
+    pid: 11,
+    lpSymbol: 'KLAY-KXRP LP',
+    lpAddresses: {
+      1001: process.env.REACT_APP_KLAY_KXRP_LP_TESTNET,
+      8217: process.env.REACT_APP_KLAY_KXRP_LP_MAINNET,
+    },
+    tokenSymbol: 'KXRP',
+    tokenAddresses: {
+      1001: process.env.REACT_APP_KXRP_ADDRESS_TESTNET,
+      8217: process.env.REACT_APP_KXRP_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.KLAY,
+    quoteTokenAdresses: contracts.wklay,
+  },
+  {
+    pid: 12,
+    lpSymbol: 'KETH-KUSDT LP',
+    lpAddresses: {
+      1001: process.env.REACT_APP_KETH_KUSDT_LP_TESTNET,
+      8217: process.env.REACT_APP_KETH_KUSDT_LP_MAINNET,
+    },
+    tokenSymbol: 'KETH',
+    tokenAddresses: {
+      1001: process.env.REACT_APP_KETH_ADDRESS_TESTNET,
+      8217: process.env.REACT_APP_KETH_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.KUSDT,
+    quoteTokenAdresses: contracts.kusdt,
+  },
+  {
+    pid: 13,
+    lpSymbol: 'KBTC-KUSDT LP',
+    lpAddresses: {
+      1001: process.env.REACT_APP_KBTC_KUSDT_LP_TESTNET,
+      8217: process.env.REACT_APP_KBTC_KUSDT_LP_MAINNET,
+    },
+    tokenSymbol: 'KBTC',
+    tokenAddresses: {
+      1001: process.env.REACT_APP_KWBTC_ADDRESS_TESTNET,
+      8217: process.env.REACT_APP_KWBTC_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.KUSDT,
+    quoteTokenAdresses: contracts.kusdt,
+  },
+  {
+    pid: 14,
+    lpSymbol: 'KXRP-KUSDT LP',
+    lpAddresses: {
+      1001: process.env.REACT_APP_KXRP_KUSDT_LP_TESTNET,
+      8217: process.env.REACT_APP_KXRP_KUSDT_LP_MAINNET,
+    },
+    tokenSymbol: 'KXRP',
+    tokenAddresses: {
+      1001: process.env.REACT_APP_KXRP_ADDRESS_TESTNET,
+      8217: process.env.REACT_APP_KXRP_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.KUSDT,
+    quoteTokenAdresses: contracts.kusdt,
+  },
+  {
+    pid: 16,
+    lpSymbol: 'KBNB-KUSDT LP',
+    lpAddresses: {
+      1001: process.env.REACT_APP_KBNB_KUSDT_LP_TESTNET,
+      8217: process.env.REACT_APP_KBNB_KUSDT_LP_MAINNET,
+    },
+    tokenSymbol: 'KBNB',
+    tokenAddresses: {
+      1001: process.env.REACT_APP_KBNB_ADDRESS_TESTNET,
+      8217: process.env.REACT_APP_KBNB_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.KUSDT,
+    quoteTokenAdresses: contracts.kusdt,
+  },
+  {
+    pid: 17,
+    lpSymbol: 'KBNB-FINIX LP',
+    lpAddresses: {
+      1001: process.env.REACT_APP_KBNB_FINIX_LP_TESTNET,
+      8217: process.env.REACT_APP_KBNB_FINIX_LP_MAINNET,
+    },
+    tokenSymbol: 'KBNB',
+    tokenAddresses: {
+      1001: process.env.REACT_APP_KBNB_ADDRESS_TESTNET,
+      8217: process.env.REACT_APP_KBNB_ADDRESS_MAINNET,
+    },
+    quoteTokenSymbol: QuoteToken.FINIX,
+    quoteTokenAdresses: contracts.finix,
+  },
 
   // {
   //   pid: 3,
