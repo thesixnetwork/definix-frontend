8b493c990ccbd1a56ee36a56c5322664a9a0e73e
Laddawal Kongwong <laddawal.kongwong>
2021-06-09 18:48:01 +0700

    Darkmode

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -9,8 +9,7 @@ const ThemeContext = React.createContext({ isDark: null, toggleTheme: () => null
 const ThemeContextProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(() => {
     const isDarkUserSetting = localStorage.getItem(CACHE_KEY)
-    // return isDarkUserSetting ? JSON.parse(isDarkUserSetting) : false
-    return true
+    return isDarkUserSetting ? JSON.parse(isDarkUserSetting) : false
   })
 
   const toggleTheme = () => {
