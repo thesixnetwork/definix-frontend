837fffef4946460b74d9860b5537a8a56d76c6f5
Tanapat Sainak <fallen_things@hotmail.com>
2021-06-21 11:13:24 +0700

    fix: Wording

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -126,7 +126,7 @@ const Home: React.FC = () => {
                   style={{ background: themes.theme.colors.primary, borderRadius: themes.theme.radii.card }}
                 />
               ) : (
-                <Caption>Put your helmet on!! We are going to the MOON!!</Caption>
+                <Caption>Klaytn farm reward will start at 3PM (GMT+7) 21 June 2021.</Caption>
               )}
             </div>
 
