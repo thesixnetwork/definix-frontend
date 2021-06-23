10c91c782b1e48fe3fe6ca428ed6880f2c6959c2
Tanapat Sainak <fallen_things@hotmail.com>
2021-06-23 15:01:58 +0700

    fix: Linting

--- src/klaytn/caver.js
+++ src/klaytn/caver.js
@@ -8,9 +8,13 @@ import Caver from 'caver-js'
 // const rpcURL = process.env.REACT_APP_KLAYTN_RPC_URL;
 // console.log("rpcURL = ", rpcURL);
 
-window.addEventListener('klaytn#initialized', () => {
-  window.caver = new Caver(window.klaytn)
-}, false)
+window.addEventListener(
+  'klaytn#initialized',
+  () => {
+    window.caver = new Caver(window.klaytn)
+  },
+  false,
+)
 // const caver = new Caver(rpcURL);
 
 export default window.klaytn
