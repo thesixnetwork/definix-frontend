1a7ef01da0f558c882d44f62e54c1efe6279ce03
nutnaree <nutnaree.kii@gmail>
2021-09-15 15:12:55 +0700

    fix: Comment file not use

--- src/views/Home/components/CardAutoRebalancing.tsx
+++ src/views/Home/components/CardAutoRebalancing.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import { Button, Card, Heading, Text } from 'uikit-dev'
 
 import lady from 'uikit-dev/images/for-ui-v2/AUTO-RE-BALANCING-MUTUAL-FUNDS.png'
-import definixLongTerm from 'uikit-dev/images/for-ui-v2/definix-long-term-stake-with-voting-system.png'
+// import definixLongTerm from 'uikit-dev/images/for-ui-v2/definix-long-term-stake-with-voting-system.png'
 // import definixWeekly from 'uikit-dev/images/for-ui-v2/definix-weekly-update.png'
 
 const StyledBannerLady = styled(Card)`
@@ -54,52 +54,52 @@ const StyledBannerLady = styled(Card)`
   }
 `
 
-const StyledBannerLongTerm = styled(Card)`
-  width: 100%;
-  background: ${({ theme }) => theme.colors.card};
-  padding: 48px 24px;
-  position: relative;
-  overflow: visible;
+// const StyledBannerLongTerm = styled(Card)`
+//   width: 100%;
+//   background: ${({ theme }) => theme.colors.card};
+//   padding: 48px 24px;
+//   position: relative;
+//   overflow: visible;
 
-  &:before {
-    content: '';
-    width: 70%;
-    height: 100%;
-    background: url(${definixLongTerm});
-    background-size: contain;
-    background-position: right bottom;
-    background-repeat: no-repeat;
-    position: absolute;
-    top: 0;
-    right: 0;
-    opacity: 0.2;
-    border-bottom-right-radius: ${({ theme }) => theme.radii.card};
-  }
+//   &:before {
+//     content: '';
+//     width: 70%;
+//     height: 100%;
+//     background: url(${definixLongTerm});
+//     background-size: contain;
+//     background-position: right bottom;
+//     background-repeat: no-repeat;
+//     position: absolute;
+//     top: 0;
+//     right: 0;
+//     opacity: 0.2;
+//     border-bottom-right-radius: ${({ theme }) => theme.radii.card};
+//   }
 
-  h2 {
-    font-size: 24px;
-  }
-  h3 {
-    font-size: 12px !important;
-    margin-bottom: 4px;
-  }
+//   h2 {
+//     font-size: 24px;
+//   }
+//   h3 {
+//     font-size: 12px !important;
+//     margin-bottom: 4px;
+//   }
 
-  ${({ theme }) => theme.mediaQueries.sm} {
-    padding: 48px 40% 48px 24px;
+//   ${({ theme }) => theme.mediaQueries.sm} {
+//     padding: 48px 40% 48px 24px;
 
-    &:before {
-      width: 40%;
-      opacity: 1;
-    }
+//     &:before {
+//       width: 40%;
+//       opacity: 1;
+//     }
 
-    h2 {
-      font-size: 32px !important;
-    }
-    h3 {
-      font-size: 16px !important;
-    }
-  }
-`
+//     h2 {
+//       font-size: 32px !important;
+//     }
+//     h3 {
+//       font-size: 16px !important;
+//     }
+//   }
+// `
 
 const SpecialButton = styled(Button)`
   position: relative;
@@ -140,47 +140,47 @@ const SpecialButton = styled(Button)`
   }
 `
 
-const SpecialOutline = styled(Button)`
-  position: relative;
-  padding: 0 40px;
-  background-color: unset;
-  overflow: hidden;
-  border-radius: 40px;
-  border: 1px solid #1587c9;
-  color: #1587c9;
+// const SpecialOutline = styled(Button)`
+//   position: relative;
+//   padding: 0 40px;
+//   background-color: unset;
+//   overflow: hidden;
+//   border-radius: 40px;
+//   border: 1px solid #1587c9;
+//   color: #1587c9;
 
-  &:before {
-    content: '';
-    width: 100%;
-    height: 100%;
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background: #f3d36c;
-    transform: scaleX(0);
-    transform-origin: 100%;
-    transition: transform 0.6s ease;
-  }
+//   &:before {
+//     content: '';
+//     width: 100%;
+//     height: 100%;
+//     display: block;
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     background: #f3d36c;
+//     transform: scaleX(0);
+//     transform-origin: 100%;
+//     transition: transform 0.6s ease;
+//   }
 
-  &:hover {
-    &:before {
-      transform-origin: 0;
-      transform: scaleX(1);
-      background: #d1eeff;
-    }
+//   &:hover {
+//     &:before {
+//       transform-origin: 0;
+//       transform: scaleX(1);
+//       background: #d1eeff;
+//     }
 
-    span {
-      color: #1587c9 !important;
-    }
-  }
+//     span {
+//       color: #1587c9 !important;
+//     }
+//   }
 
-  span {
-    position: relative;
-    z-index: 1;
-    transition: all 0.6s ease;
-  }
-`
+//   span {
+//     position: relative;
+//     z-index: 1;
+//     transition: all 0.6s ease;
+//   }
+// `
 
 const CardAutoRebalancing = ({ className = '' }) => {
   // const openDate = new Date(1628841600000)
@@ -209,7 +209,7 @@ const CardAutoRebalancing = ({ className = '' }) => {
           </SpecialButton>
         </div>
       </StyledBannerLady>
-      <StyledBannerLongTerm className={className}>
+      {/* <StyledBannerLongTerm className={className}>
         <div className="pos-relative" style={{ zIndex: 1 }}>
           <Heading className="mb-2" color="primary">
             FINIX LONG-TERM STAKE WITH VOTING SYSTEM
@@ -221,15 +221,12 @@ const CardAutoRebalancing = ({ className = '' }) => {
               Rebalancing Farm.
             </b>
           </Text>
-          {/* <Text bold fontSize="12px">
-            vFINIX can vote for rewarding fee return from a liquidity pool and Discount fee conditioning for Rebalancing
-            Farm.
-          </Text> */}
+
           <SpecialOutline className="mt-4">
             <span>Coming soon!</span>
           </SpecialOutline>
         </div>
-      </StyledBannerLongTerm>
+      </StyledBannerLongTerm> */}
     </>
   )
 }
