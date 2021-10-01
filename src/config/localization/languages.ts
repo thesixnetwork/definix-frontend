5b5508bf4ce77513e23ef4ce5bca6dd916ebe6a6
nutnaree <nutnaree.kii@gmail>
2021-10-01 15:21:27 +0700

    fix: Used 3 language

--- src/config/localization/languages.ts
+++ src/config/localization/languages.ts
@@ -7,13 +7,11 @@ export interface Language {
 export const EN: Language = { locale: 'en-US', language: 'English', code: 'en' }
 export const KO: Language = { locale: 'ko-KR', language: '한국어', code: 'ko' }
 export const TH: Language = { locale: 'th-TH', language: 'ภาษาไทย', code: 'th' }
-export const ZHCN: Language = { locale: 'zh-CN', language: '简体中文', code: 'zh-cn' }
 
 export const languages = {
   'en-US': EN,
   'ko-KR': KO,
   'th-TH': TH,
-  'zh-CN': ZHCN,
 }
 
 export const languageList = Object.values(languages)
