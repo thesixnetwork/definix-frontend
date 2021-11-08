2b339b9a9d710c0769b64130a7d6979f9b9ec3f4
lotus1128 <50655406+lotus1128@users.noreply.github.com>
2021-11-09 02:39:18 +0900

    feat: Edit menu config

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,39 +1,48 @@
 import React from 'react'
-// import { useWallet } from '@sixnetwork/klaytn-use-wallet'
+import { useWallet } from '@sixnetwork/klaytn-use-wallet'
 import { supportedLanguages } from 'config/localisation/languageCodes'
 import useTheme from 'hooks/useTheme'
-import { usePriceFinixUsd, useProfile } from 'state/hooks'
+// import { usePriceFinixUsd, useProfile } from 'state/hooks'
 import { Menu as UikitMenu } from 'definixswap-uikit'
-import { useTranslation } from 'react-i18next'
-import SettingsModal from 'views/Explore/components/SettingsModal'
+import { useTranslation, Trans } from 'react-i18next'
+import useUserSlippageTolerance from 'hooks/useUserSlippageTolerance'
+import useUserDeadline from 'hooks/useUserDeadline'
+import NetWorth from './NetWorth'
 import { links } from './config'
-import UserBlock from './UserBlock'
-import Chain from './Chain'
-// import { Menu as UikitMenu } from 'uikit-dev'
 
 const Menu = (props) => {
-  // const { account, connect, reset } = useWallet()
+  const { account, connect, reset } = useWallet()
   const { i18n, t } = useTranslation()
+  const [userSlippageTolerance, setUserslippageTolerance] = useUserSlippageTolerance()
+  const [deadline, setDeadline] = useUserDeadline()
   const { isDark, toggleTheme } = useTheme()
-  const finixPriceUsd = usePriceFinixUsd()
+  // const finixPriceUsd = usePriceFinixUsd()
   // const { profile } = useProfile()
 
   return (
     <UikitMenu
-      userBlock={<UserBlock />}
-      chain={<Chain />}
-      settingModal={<SettingsModal />}
-      // account={account}
-      // login={connect}
-      // logout={reset}
-      isDark={isDark}
-      toggleTheme={toggleTheme}
+      // SettingsModal slippage
+      userSlippageTolerance={userSlippageTolerance}
+      setUserslippageTolerance={setUserslippageTolerance}
+      // SettingsModal deadline
+      deadline={deadline}
+      setDeadline={setDeadline}
+
+      account={account}
+      login={connect}
+      logout={reset}
+      netWorth={<NetWorth />}
+
+      Trans={Trans}
       currentLang={i18n.language}
       langs={supportedLanguages}
       setLang={({ code }) => i18n.changeLanguage(code)}
+
+      isDark={isDark}
+      toggleTheme={toggleTheme}
+      links={links(t)}
       // finixPriceUsd={finixPriceUsd.toNumber()}
       // price={finixPriceUsd.toNumber() <= 0 ? 'N/A' : numeral(finixPriceUsd.toNumber()).format('0,0.0000')}
-      links={links(t)}
       // profile={{
       //   username: profile?.username,
       //   image: profile?.nft ? `/images/nfts/${profile.nft?.images.sm}` : undefined,
