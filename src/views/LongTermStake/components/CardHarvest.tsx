0eca388b015bb1bcba59bb63099e228c599a7599
Nutnaree <nutnaree@six.network>
2021-11-12 17:38:04 +0700

    fix: Update style my asset

--- src/views/LongTermStake/components/CardHarvest.tsx
+++ src/views/LongTermStake/components/CardHarvest.tsx
@@ -50,21 +50,21 @@ const CardHarvest = () => {
         My Asset
       </Heading>
       <div className={`${isMobile ? 'col-12 pa-4' : 'col-12 flex bd-b py-4 px-3 '}`}>
-        <div className={`${isMobile ? 'col-12 mb-4' : 'col-3 pa-2'}`}>
+        <div className={`align-self-center ${isMobile ? 'col-12 mb-4' : 'col-3 pa-2'}`}>
           <Text color="textSubtle">Your vFINIX balance</Text>
           <Heading as="h1" style={{ lineHeight: 2 }} fontSize="18px !important">
             {numeral(balancevfinix).format('0,0.[000]')} vFINIX
           </Heading>
         </div>
         <div className="bd-r" />
-        <div className={`${isMobile ? 'col-12 mb-4' : 'col-3 py-2 pl-4 '}`}>
+        <div className={`align-self-center ${isMobile ? 'col-12 mb-4' : 'col-3 py-2 pl-4 '}`}>
           <Text color="textSubtle">Your total FINIX stake</Text>
           <Heading as="h1" style={{ lineHeight: 2 }} fontSize="18px !important">
             {numeral(lockAmount).format('0,0.[000]')} FINIX
           </Heading>
         </div>
         <div className="bd-r" />
-        <div className={`${isMobile ? 'col-12' : 'col-3 pa-2 pl-4'}`}>
+        <div className={`align-self-center ${isMobile ? 'col-12' : 'col-3 pa-2 pl-4'}`}>
           <Text color="textSubtle">Your FINIX earned</Text>
           <div className={`${isMobile ? 'flex' : 'flex'}`}>
             <Heading as="h1" style={{ lineHeight: 2 }} fontSize="18px !important" color="success">
@@ -76,7 +76,7 @@ const CardHarvest = () => {
             </Heading>
           </div>
         </div>
-        <div className={`${isMobile ? 'col-12 pt-5' : 'col-3 pa-2 align-self-center'}`}>
+        <div className={`align-self-center ${isMobile ? 'col-12 pt-5' : 'col-3 pa-2 align-self-center'}`}>
           <Button variant="success" radii="small" disabled={!valueReward} onClick={onHarvest} fullWidth>
             Harvest
           </Button>
