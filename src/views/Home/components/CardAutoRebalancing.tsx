5c16efd27541fe2031460aedaa42bbde0551146d
Nutnaree <nutnaree@six.network>
2021-11-18 18:29:07 +0700

    fix: Update position img banner

--- src/views/Home/components/CardAutoRebalancing.tsx
+++ src/views/Home/components/CardAutoRebalancing.tsx
@@ -227,8 +227,9 @@ const StyledBannerDingoXSix = styled(Card)`
     border-radius: unset;
     height: 327px;
     &:before {
-      width: 46%;
+      width: 42%;
       opacity: 1;
+      background-position: center;
     }
     h2 {
       font-size: 28px !important;
