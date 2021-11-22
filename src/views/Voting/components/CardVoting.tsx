a7289a607f3499018d464a0c64b3e34fe0217c81
toonnpatchara <patchara.a@six.network>
2021-11-22 15:35:36 +0700

    fix: Update Make a Proposals

--- src/views/Voting/components/CardVoting.tsx
+++ src/views/Voting/components/CardVoting.tsx
@@ -7,26 +7,11 @@ import definixVoting from 'uikit-dev/images/for-ui-v2/voting-banner.png'
 import CardProposals from './CardProposals'
 
 const StyledBannerVoting = styled(Card)`
+  display: flex;
   width: 100%;
   background: ${({ theme }) => theme.colors.card};
-  padding: 48px 24px;
-  position: relative;
-  overflow: visible;
-
-  &:before {
-    content: '';
-    width: 70%;
-    height: 100%;
-    background: url(${definixVoting});
-    background-size: contain;
-    background-position: right bottom;
-    background-repeat: no-repeat;
-    position: absolute;
-    top: 0;
-    right: 0;
-    opacity: 0.2;
-    // border-bottom-right-radius: ${({ theme }) => theme.radii.card};
-  }
+  padding: 0px 42px 0px 42px;
+  align-items: center;
 
   h2 {
     font-size: 24px;
@@ -36,14 +21,6 @@ const StyledBannerVoting = styled(Card)`
   }
 
   ${({ theme }) => theme.mediaQueries.sm} {
-    padding: 48px 42% 42px 66px;
-    // height: 214px;
-
-    &:before {
-      width: 26%;
-      opacity: 1;
-    }
-
     h2 {
       font-size: 32px !important;
     }
@@ -57,11 +34,14 @@ const CardVoting = () => {
   return (
     <>
       <StyledBannerVoting className="mt-5">
-        <Heading color="primary">DRIVE FORWARD TOGETHER WITH DECENTRALIZED VOTING</Heading>
-        <Text>
-          Community Proposal is a great way to say your words and to reflects the community feeling about your ideas.
-        </Text>
-        <Button as={Link} to="/voting/make-proposal" radii="small" color="success">
+        <div>
+          <Heading color="primary">DRIVE FORWARD TOGETHER WITH DECENTRALIZED VOTING</Heading>
+          <Text>
+            Community Proposal is a great way to say your words and to reflects the community feeling about your ideas.
+          </Text>
+        </div>
+        <img style={{ backgroundSize: 'contain' }} alt="" src={definixVoting} width="250" height="250" />
+        <Button style={{ width: '26%' }} as={Link} to="/voting/make-proposal" radii="small" color="success">
           Make a Proposals
         </Button>
       </StyledBannerVoting>
