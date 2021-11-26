5ebb4c40f59243c94406faa80425a611e0d77585
NANTANART TAKONUAN <nantanart@six.network>
2021-11-26 18:42:09 +0700

    fix: Remove usdToken from array

--- src/views/Explore/Withdraw.tsx
+++ src/views/Explore/Withdraw.tsx
@@ -478,7 +478,6 @@ const Withdraw: React.FC<WithdrawType> = ({ rebalance }) => {
         _.compact([...((rebalance || {}).tokens || [])]).map((c, index) => {
           const ratioPoint = (
             ((rebalance || {}).tokenRatioPoints || [])[index] ||
-            ((rebalance || {}).usdTokenRatioPoint || [])[0] ||
             new BigNumber(0)
           ).toNumber()
           const ratioObject = ((rebalance || {}).ratio || []).find((r) => r.symbol === c.symbol)
