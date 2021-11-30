83b64f7954f1701ba57a388e6b3a3fa132bd1487
Tanapat Sainak <fallen_things@hotmail.com>
2021-11-30 09:43:06 +0700

    fix: Add pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -126,6 +126,26 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
+  {
+    sousId: 25,
+    tokenName: 'SIX',
+    stakingTokenName: QuoteToken.SIX,
+    stakingTokenAddress:
+      process.env.REACT_APP_CHAIN_ID === '97'
+        ? process.env.REACT_APP_SIX_ADDRESS_TESTNET
+        : process.env.REACT_APP_SIX_ADDRESS_MAINNET,
+    contractAddress: {
+      97: process.env.REACT_APP_HERODOTUS_TESTNET,
+      56: process.env.REACT_APP_HERODOTUS_MAINNET,
+    },
+    poolCategory: PoolCategory.CORE,
+    projectLink: 'https://definix.com/',
+    harvest: true,
+    tokenPerBlock: '10',
+    sortOrder: 1,
+    isFinished: false,
+    tokenDecimals: 18,
+  },
 ]
 
 export default pools
