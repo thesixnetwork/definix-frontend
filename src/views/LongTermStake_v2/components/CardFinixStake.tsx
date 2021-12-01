f8c25afe9e9bd044eb0adeb145625e8cf3ac335c
zeroaan <yj6210ag@gmail.com>
2021-12-01 15:58:34 +0900

    feat: Add vFinix APR Button mobile

--- src/views/LongTermStake_v2/components/CardFinixStake.tsx
+++ src/views/LongTermStake_v2/components/CardFinixStake.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Card, Flex } from 'definixswap-uikit'
 import styled from 'styled-components'
 
-import VFinixAPR from './VFinixAPR'
+import VFinixApr from './VFinixApr'
 import { IsMobileType } from './types'
 
 const FlexCard = styled(Flex)`
@@ -13,11 +13,32 @@ const FlexCard = styled(Flex)`
 const CardFinixStake: React.FC<IsMobileType> = ({ isMobile }) => {
   const [days, setDays] = useState('365 days')
 
+  const data = [
+    {
+      multiple: '1X',
+      day: '90 days',
+      apr: '27.69',
+      minStake: '1,000',
+    },
+    {
+      multiple: '2X',
+      day: '180 days',
+      apr: '55.30',
+      minStake: '10,000',
+    },
+    {
+      multiple: '4X',
+      day: '365 days',
+      apr: '110.76',
+      minStake: '30,000',
+    },
+  ]
+
   return (
     <>
       <Card p={isMobile ? 'S_20' : 'S_40'} mt="S_16">
         <FlexCard>
-          <VFinixAPR isMobile={isMobile} days={days} setDays={setDays} />
+          <VFinixApr isMobile={isMobile} days={days} setDays={setDays} data={data} />
         </FlexCard>
       </Card>
     </>
