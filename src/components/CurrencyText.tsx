eb45d6d02817e7a763918eec1cbd5b8a333899ec
specialyoung <kimsy89@mhand.net>
2021-12-13 17:27:34 +0900

    chore: Add toFixed props to currencyText

--- src/components/CurrencyText.tsx
+++ src/components/CurrencyText.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import numeral from 'numeral'
 import { Text } from 'definixswap-uikit-v2'
 
-const CurrencyText: React.FC<any> = (props) => {
-  const { value, prefix, ...rest } = props
+const CurrencyText: React.FC<{
+  value: number
+  prefix?: string
+  toFixed?: number
+  [key: string]: any
+}> = (props) => {
+  const { value, prefix, toFixed = 2, ...rest } = props
   return (
     <Text {...rest}>
-      {prefix || ''} {`$${numeral(value).format('0,0.[00]')}`}
+      {prefix || ''} {`$${numeral(value).format(`0,0.[${'0'.repeat(toFixed)}]`)}`}
     </Text>
   )
 }
