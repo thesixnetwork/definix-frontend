a6047dda84757dd061c57b332719650c70f6cb3d
Jinyoung Ahn <yj6210ag@gmail.com>
2021-12-14 11:32:24 +0900

    chore: Add LongTermStake how to link

--- src/views/LongTermStake_v2/components/TitleStake.tsx
+++ src/views/LongTermStake_v2/components/TitleStake.tsx
@@ -12,7 +12,7 @@ const TitleStake: React.FC = () => {
           title={t('Long-term Stake')}
           description={t('Stake FINIX to earn vFINIX')}
           linkLabel={t('Learn how to Long-term stake')}
-          link="/long-term-stake"
+          link="https://sixnetwork.gitbook.io/definix-on-klaytn-en/long-term-staking-pool/how-to-stake-in-long-term-staking-pool"
           linkBottom
         />
       </Flex>
