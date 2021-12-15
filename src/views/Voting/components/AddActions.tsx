47c5641e55b3fab0b105476c395d9f3a3fcbcbf7
toonnpatchara <patchara.a@six.network>
2021-12-15 16:36:29 +0700

    fix: Add DatePicker

--- src/views/Voting/components/AddActions.tsx
+++ src/views/Voting/components/AddActions.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-nested-ternary */
-import React from 'react'
+import React, { useState } from 'react'
 import { ArrowBackIcon, Button, Card, Text, Input, useMatchBreakpoints } from 'uikit-dev'
 import { Link, Redirect } from 'react-router-dom'
 import { ExternalLink } from 'react-feather'
@@ -7,6 +7,7 @@ import styled from 'styled-components'
 import useTheme from 'hooks/useTheme'
 import Checkbox from '@material-ui/core/Checkbox'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { DatePicker, TimePicker } from 'components/DatePicker'
 import exploreIcon from '../../../uikit-dev/images/for-ui-v2/voting/icon-explore.png'
 // import development from '../../../uikit-dev/images/for-ui-v2/voting/voting-development.png'
 
@@ -20,10 +21,64 @@ const BoxInput = styled(Input)`
   align-items: center;
 `
 
+export interface FormState {
+  name: string
+  body: string
+  // choices: Choice[]
+  startDate: Date
+  startTime: Date
+  endDate: Date
+  endTime: Date
+  snapshot: number
+}
+
+const Custom = ({ value, onClick, ...rest }) => {
+  return (
+    <input
+      style={{
+        backgroundColor: 'rgb(238, 234, 244)',
+        borderRadius: '16px',
+        boxShadow: 'rgb(74 74 104 / 10%) 0px 2px 2px -1px inset',
+        color: 'rgb(40, 13, 95)',
+        display: 'block',
+        fontSize: '16px',
+        height: '40px',
+        outline: '0px',
+        padding: '0px 16px',
+        width: '100%',
+        border: '1px solid rgb(215, 202, 236)',
+      }}
+      value={value}
+      onClick={onClick}
+      {...rest}
+    />
+  )
+}
+
 const AddActions = () => {
   const { isDark } = useTheme()
   const { isXl, isLg } = useMatchBreakpoints()
   const isMobile = !isXl && !isLg
+  // const [startDate, setStartDate] = useState(new Date())
+  const [state, setState] = useState<FormState>({
+    name: '',
+    body: '',
+    // choices: times(MINIMUM_CHOICES).map(makeChoice),
+    startDate: null,
+    startTime: null,
+    endDate: null,
+    endTime: null,
+    snapshot: 0,
+  })
+  const { name, body, startDate, startTime, endDate, endTime, snapshot } = state
+
+  const handleDateChange = (key: string) => (value: Date) => {
+    updateValue(key, value)
+  }
+
+  const updateValue = (key: string, value: string | Date) => {
+    console.log('updateValue')
+  }
 
   return (
     <>
@@ -38,7 +93,12 @@ const AddActions = () => {
             <Text color="text" fontSize="16px">
               Start Date
             </Text>
-            <BoxInput className="my-2" placeholder="DD-MM-YY" />
+            <DatePicker
+              name="startDate"
+              onChange={handleDateChange('startDate')}
+              selected={startDate}
+              placeholderText="YYYY/MM/DD"
+            />
           </div>
           <div className="mb-3">
             <Text color="text" fontSize="16px">
