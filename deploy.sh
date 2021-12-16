a669728a1193e1ba5b2a29e50b8772ab73bfbfa8
lotus1128 <50655406+lotus1128@users.noreply.github.com>
2021-12-16 16:47:16 +0900

    feat: Edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -23,11 +23,11 @@ _exit_if_fail $?
 yarn build:${DEPLOY_ENV}
 _exit_if_fail $?
 
-aws s3 sync ./build s3://${WEBS3} --acl public-read --profile ${2}
+aws --profile ${2} s3 sync ./build s3://${WEBS3}
 _exit_if_fail $?
 
 echo "Clear cache...."
-aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths '/*'  --profile ${2}
+aws --profile ${2} cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*"
 _exit_if_fail $?
 
 echo "https://${WEBS3}.s3-ap-southeast-1.amazonaws.com/index.html"
