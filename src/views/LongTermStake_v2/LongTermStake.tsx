9992c709feaf183d27c1e7508cd4996b677f001a
Jinyoung Ahn <yj6210ag@gmail.com>
2021-12-20 18:34:20 +0900

    chore: Change LongTermStake working wrap

--- src/views/LongTermStake_v2/LongTermStake.tsx
+++ src/views/LongTermStake_v2/LongTermStake.tsx
@@ -1,41 +1,41 @@
 import React from 'react'
-import { Box, useMatchBreakpoints } from '@fingerlabs/definixswap-uikit-v2'
+import { Box, Flex, Text, Button, useMatchBreakpoints } from '@fingerlabs/definixswap-uikit-v2'
 import { useWallet } from '@sixnetwork/klaytn-use-wallet'
 
 import styled from 'styled-components'
-// import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import TitleStake from './components/TitleStake'
 import CardTotalStake from './components/CardTotalStake'
 import CardTotalEarn from './components/CardTotalEarn'
 import CardFinixStake from './components/CardFinixStake'
 import CardStakeList from './components/CardStakeList'
 
-// import repairImgX1 from '../../assets/images/img_longterm_repair.png'
-// import repairImgX2 from '../../assets/images/img_longterm_repair@2x.png'
-// import repairImgX3 from '../../assets/images/img_longterm_repair@3x.png'
+import repairImgX1 from '../../assets/images/img_longterm_repair.png'
+import repairImgX2 from '../../assets/images/img_longterm_repair@2x.png'
+import repairImgX3 from '../../assets/images/img_longterm_repair@3x.png'
 
 const Wrap = styled.div`
   position: relative;
 `
 
-// const Working = styled(Flex)`
-//   background-color: rgba(255, 255, 255, 0.9);
-//   position: absolute;
-//   left: 0;
-//   top: 0;
-//   width: 100%;
-//   height: 100%;
-//   border-radius: 16px;
-//   white-space: pre-line;
-//   text-align: center;
-//   padding: 40px;
-//   padding-top: 140px;
-// `
+const Working = styled(Flex)`
+  background-color: rgba(255, 255, 255, 0.9);
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 16px;
+  white-space: pre-line;
+  text-align: center;
+  padding: 40px;
+  padding-top: 140px;
+`
 
 const LongTermStake: React.FC = () => {
   const { isMobile } = useMatchBreakpoints()
   const { account } = useWallet()
-  // const { t } = useTranslation()
+  const { t } = useTranslation()
 
   return (
     <>
@@ -43,10 +43,10 @@ const LongTermStake: React.FC = () => {
         <TitleStake />
         <Wrap>
           <CardTotalStake isMobile={isMobile} />
-          {!!account && <CardTotalEarn isMobile={isMobile} />}
           <CardFinixStake isMobile={isMobile} />
+          {!!account && <CardTotalEarn isMobile={isMobile} />}
           {!!account && <CardStakeList isMobile={isMobile} />}
-          {/* <Working p="S_32" flexDirection="column" alignItems="center">
+          <Working p="S_32" flexDirection="column" alignItems="center">
             <img alt="" src={repairImgX1} srcSet={`${repairImgX2} x2, ${repairImgX3} x3`} />
             <Text textStyle="R_16M" mb="S_16" mt="30px">
               {t('Still in progress')}
@@ -54,7 +54,7 @@ const LongTermStake: React.FC = () => {
             <Button width="140px" onClick={() => window.open('https://klaytn.definix.com/long-term-stake')}>
               {t('Go to G1')}
             </Button>
-          </Working> */}
+          </Working>
         </Wrap>
       </Box>
     </>
