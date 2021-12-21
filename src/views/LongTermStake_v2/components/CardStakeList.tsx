e05345be68b9ca540150425d3630751d57cc624d
Jinyoung Ahn <yj6210ag@gmail.com>
2021-12-21 16:35:45 +0900

    chore: Change LongTermStake stake list [GZ2021-440] [GZ2021-445]

--- src/views/LongTermStake_v2/components/CardStakeList.tsx
+++ src/views/LongTermStake_v2/components/CardStakeList.tsx
@@ -22,7 +22,7 @@ const CardStakeList: React.FC<IsMobileType> = ({ isMobile }) => {
   const [dataLength, setDataLength] = useState<number>(0)
 
   useEffect(() => {
-    setItemPerPage(isMobile ? 3 : 4)
+    setItemPerPage(isMobile ? 3 : 5)
   }, [isMobile])
 
   useEffect(() => {
--- src/views/LongTermStake_v2/components/StakeListContentPc.tsx
+++ src/views/LongTermStake_v2/components/StakeListContentPc.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import numeral from 'numeral'
 import { useTranslation } from 'react-i18next'
-import { Flex, Text, Divider } from '@fingerlabs/definixswap-uikit-v2'
+import { Flex, Text, Divider, Helper } from '@fingerlabs/definixswap-uikit-v2'
 
 import UnstakeButton from './UnstakeButton'
 import { AllDataLockType, IsMobileType } from './types'
@@ -19,19 +19,31 @@ const StakeListContentPc: React.FC<ContentProps> = ({ isMobile, allDataLock }) =
         return (
           <Flex width="100%" flexDirection="column" key={item.id}>
             <Flex width="100%" alignItems="center" py="S_16">
-              <Text width="20%" textStyle="R_14R" color="black">
-                {item.days} {t('days')}
-              </Text>
-              <Text width="20%" textStyle="R_14R" color="black">
+              <Flex width="20%" flexDirection="column">
+                <Text textStyle="R_14R" color="black">
+                  {item.days} {t('days')}
+                </Text>
+                {item.topup.some((topup: any) => Number(topup) === item.id) && (
+                  <>
+                    <Flex mt="S_2">
+                      <Text textStyle="R_12R" color="red" mr="S_4">
+                        Super staked
+                      </Text>
+                      <Helper text="Super Stake is a feature that can harvest all of your FINIX reward to stake in Long-term stake with no minimum amount. You can stake as much as FINIX you prefer under the same lock period within 28 days, your lock period will not be extended." />
+                    </Flex>
+                  </>
+                )}
+              </Flex>
+              <Text width="27%" textStyle="R_14R" color="black">
                 {numeral(item.lockAmount).format(0, 0)}
               </Text>
-              <Flex width="60%" justifyContent="space-between">
+              <Flex width="53%" justifyContent="space-between">
                 <Flex flexDirection="column" justifyContent="center">
                   <Text textStyle="R_14R" color="black">
-                    {item.lockTimestamp} GMT+9
+                    {item.lockTimestamp}
                   </Text>
                   <Text textStyle="R_12R" color="mediumgrey">
-                    {t('*Asia/Seoul')}
+                    *GMT +9 {t('Asia/Seoul')}
                   </Text>
                 </Flex>
                 <UnstakeButton isMobile={isMobile} data={item} />
