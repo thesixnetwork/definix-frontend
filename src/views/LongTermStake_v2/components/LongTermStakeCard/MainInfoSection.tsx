5b5393d50c0bbb3ce8b1050339e89cf6605a58a3
Jinyoung Ahn <yj6210ag@gmail.com>
2021-12-22 18:33:20 +0900

    chore: Change MyInvestment longterm apr [GZ2021-471]

--- src/views/LongTermStake_v2/components/LongTermStakeCard/MainInfoSection.tsx
+++ src/views/LongTermStake_v2/components/LongTermStakeCard/MainInfoSection.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import _ from 'lodash'
+import numeral from 'numeral'
 import styled from 'styled-components'
 // import useConverter from 'hooks/useConverter'
 import { Flex, Box, Text, Coin } from '@fingerlabs/definixswap-uikit-v2'
+import { useApr, usePrivateData } from 'hooks/useLongTermStake'
 // import ApyButton from './ApyButton'
+
+import { AllDataLockType } from '../types'
 
 const StyledCoin = styled(Coin)`
   width: 40px;
@@ -22,6 +27,25 @@ const MainInfoSection: React.FC = () => {
   //     return '-'
   //   }
   // }, [convertToPoolAPRFormat, apy])
+  const { allDataLock } = usePrivateData()
+  const apr = useApr()
+
+  const getAPR = useCallback(() => {
+    const data = allDataLock.filter((item: AllDataLockType) => !item.isUnlocked)
+    const dataLength = data.length
+    let multiple = 1
+
+    if (dataLength === 0) return [0, 0]
+    if (dataLength === 1) return [1, numeral(_.get(data, '0').multiplier * apr).format('0,0.[00]')]
+
+    data.forEach((item) => {
+      if (_.get(item, 'multiplier') > multiple) {
+        multiple = _.get(item, 'multiplier')
+      }
+    })
+
+    return [dataLength, numeral(multiple * apr).format('0,0.[00]')]
+  }, [allDataLock, apr])
 
   return (
     <Flex position="relative" alignItems="center">
@@ -32,14 +56,14 @@ const MainInfoSection: React.FC = () => {
       <Flex flexDirection="column">
         <Text textStyle="R_18M">Long-term Stake</Text>
 
-        {/* <Flex alignItems="end">
-          <Text textStyle="R_14M" color={ColorStyles.ORANGE} style={{ paddingBottom: '2px' }}>
-            APR
+        <Flex alignItems="end">
+          <Text textStyle="R_14M" color="orange" style={{ paddingBottom: '2px' }}>
+            {getAPR()[0] < 2 ? 'APR' : 'APR up to'}
           </Text>
-          <Text textStyle="R_18B" color={ColorStyles.ORANGE} style={{ marginLeft: '4px' }}>
-            {displayApy}
+          <Text textStyle="R_18B" color="orange" style={{ marginLeft: '4px' }}>
+            {getAPR()[1]}%
           </Text>
-        </Flex> */}
+        </Flex>
       </Flex>
     </Flex>
   )
