562d99a2e56229873f57ba28a3bb5c710d4fc0d9
kanthakran-six <kanthakarn@six.network>
2022-01-07 15:24:43 +0700

    fix: Add step deploy with github Action

---
+++ deploy-workflow.sh
@@ -0,0 +1,34 @@
+echo "./deploy.sh {env} {profile}"
+DEPLOY_ENV=$1
+
+function _exit_if_fail
+{
+  if [ "$1" != "0" ]; then
+    exit 1
+  fi
+}
+
+. ./.env.${DEPLOY_ENV}
+
+echo "Web S3 : ${WEBS3}"
+echo "DISTRIBUTION ID : ${DISTRIBUTION_ID}"
+echo "Profile : ${2}"
+
+echo -e "Enter to continue...\c"
+read
+
+yarn
+_exit_if_fail $?
+
+yarn build:${DEPLOY_ENV}
+_exit_if_fail $?
+
+aws s3 sync ./build s3://${WEBS3} --acl public-read --profile ${2}
+_exit_if_fail $?
+
+echo "Clear cache...."
+aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths '/*'  --profile ${2}
+_exit_if_fail $?
+
+echo "https://${WEBS3}.s3-ap-southeast-1.amazonaws.com/index.html"
+
