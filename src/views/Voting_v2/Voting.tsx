37fb06bfc0fb3dcc6d390cd36a34f027fb925526
lotus1128 <50655406+lotus1128@users.noreply.github.com>
2022-01-07 12:17:46 +0900

    chore: Add voting button GZ2021-586

--- src/views/Voting_v2/Voting.tsx
+++ src/views/Voting_v2/Voting.tsx
@@ -29,7 +29,7 @@ const Voting: React.FC = () => {
           <Flex flexDirection="column" alignItems="center" justifyContent="center" mt="120px" mb="130px">
             <ImageSet srcSet={[ImgLongtermRepair1x, ImgLongtermRepair2x, ImgLongtermRepair3x]} alt="" width={236} height={147} />
             <Text textStyle="R_14R" color="black" mt="43px" textAlign="center">{t("still in progress")}</Text>
-            <Button mt="24px" minWidth="146px">{t("Go to G1")}</Button>
+            <Button mt="24px" minWidth="146px" onClick={() => window.open('https://klaytn.definix.com/voting', '_blank')}>{t("Go to G1")}</Button>
           </Flex>
         </Card>
       </Box>
