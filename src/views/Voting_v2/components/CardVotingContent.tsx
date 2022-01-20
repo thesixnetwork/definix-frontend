c4a128bc725d910d599fc1eff8e5ba958b4761f7
lotus1128 <50655406+lotus1128@users.noreply.github.com>
2022-01-20 19:14:25 +0900

    feat: Edit vote soon startdate GZ2021-698

--- src/views/Voting_v2/components/CardVotingContent.tsx
+++ src/views/Voting_v2/components/CardVotingContent.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 import { useTranslation } from 'react-i18next'
+import dayjs from 'dayjs'
 import { Card, CardBody, Box, Flex, Text } from '@fingerlabs/definixswap-uikit-v2'
 import ReactMarkdown from 'components/ReactMarkdown'
 import useVoteTranslate from 'hooks/useVoteTranslate'
@@ -99,6 +100,7 @@ const WrapContent = styled(Flex)`
 
 const CardVotingContent: React.FC<Props> = ({ proposalIndex, proposal, participatedProposal }) => {
   const { t, i18n } = useTranslation()
+  const isStartDate = useMemo(() => dayjs().isBefore(dayjs(proposal.startEpoch)), [proposal.startEpoch])
 
   return (
     <WrapCard>
@@ -110,8 +112,8 @@ const CardVotingContent: React.FC<Props> = ({ proposalIndex, proposal, participa
           </Flex>
           <TextTitle>{useVoteTranslate(proposal.title, 'title')}</TextTitle>
           <TextEndDate>
-            <span>{t('End Date')}</span>
-            <span>{getDateFormat(i18n.languages[0], proposal.endEpoch)}</span>
+            <span>{isStartDate ? t('Start Date') : t('End Date')}</span>
+            <span>{getDateFormat(i18n.languages[0], isStartDate ? proposal.startEpoch : proposal.endEpoch)}</span>
           </TextEndDate>
           <BoxContent>
             <Text className="text">
