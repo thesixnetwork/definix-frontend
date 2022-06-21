eb1a91a192e7397ef44d5d7450d0b77ab2412c62
Nutnaree <nutnaree@six.network>
2022-06-21 11:19:39 +0700

    fix: Add banner SIX Protocol and edit style carousel

--- src/views/Home/components/CardAutoRebalancing.tsx
+++ src/views/Home/components/CardAutoRebalancing.tsx
@@ -4,8 +4,6 @@ import styled from 'styled-components'
 import { Button, Card, Heading, Text, useMatchBreakpoints } from 'uikit-dev'
 import Slider from 'react-slick'
 import useTheme from 'hooks/useTheme'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import definixLongTerm from 'uikit-dev/images/for-ui-v2/banner/definix-long-term-stake-with-voting-system.png'
 import definixVoting from 'uikit-dev/images/for-ui-v2/banner/voting-banner.png'
 import velo from 'uikit-dev/images/for-ui-v2/banner/velo-banner.png'
@@ -13,19 +11,87 @@ import dingoxSix from 'uikit-dev/images/for-ui-v2/banner/dingoxsix.png'
 import logoDingoxSixBlack from 'uikit-dev/images/for-ui-v2/banner/logo-dingoxsix-black.png'
 import logoDingoxSixWhite from 'uikit-dev/images/for-ui-v2/banner/logo-dingoxsix-white.png'
 import logoRebalance from 'uikit-dev/images/for-ui-v2/banner/logo-rebalance.png'
-import logoFinixBannerFull from 'uikit-dev/images/for-ui-v2/banner/logo-banner-finix.png'
+import logoFinixHavingBanner from 'uikit-dev/images/for-ui-v2/banner/logo-banner-finix.png'
+import logoDefinixSixProtocol from 'uikit-dev/images/for-ui-v2/banner/logo-definix-six-protocol.png'
 
-const StyledBannerLongTerm = styled(Card)`
+const StyledBannerFinixToken = styled(Card)`
   width: 100%;
   background: ${({ theme }) => theme.colors.card};
   padding: 48px 24px;
   position: relative;
   overflow: visible;
+  height: 326px;
+
   &:before {
     content: '';
     width: 70%;
     height: 100%;
-    background: url(${definixLongTerm});
+    background: ${({ theme }) => (theme.isDark ? `url(${logoFinixHavingBanner})` : `url(${logoFinixHavingBanner})`)};
+    background-size: contain;
+    background-position: right center;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    right: 0;
+    opacity: 0.2;
+    border-bottom-right-radius: ${({ theme }) => theme.radii.card};
+  }
+
+  h2 {
+    font-size: 28px;
+  }
+
+  h3 {
+    font-size: 14px !important;
+    margin-bottom: 4px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.xs} {
+    padding: 100px 24px;
+    height: 300px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    padding: 90px 40% 48px 24px;
+    border-radius: unset;
+    height: 304px;
+
+    &:before {
+      width: 40%;
+      opacity: 1;
+      margin-right: 40px;
+    }
+
+    h2 {
+      font-size: 42px !important;
+    }
+
+    h3 {
+      font-size: 18px !important;
+    }
+  }
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    padding: 90px 40% 48px 50px !important;
+    &:before {
+      margin-right: 50px !important;
+    }
+  }
+`
+
+const StyledBannerRebalance = styled(Card)`
+  width: 100%;
+  background: ${({ theme }) => theme.colors.card};
+  padding: 48px 24px;
+  position: relative;
+  overflow: visible;
+  height: 326px;
+
+  &:before {
+    content: '';
+    width: 100%;
+    height: 100%;
+    background: url(${logoRebalance});
     background-size: contain;
     background-position: right bottom;
     background-repeat: no-repeat;
@@ -35,21 +101,31 @@ const StyledBannerLongTerm = styled(Card)`
     opacity: 0.2;
     border-bottom-right-radius: ${({ theme }) => theme.radii.card};
   }
+
   h2 {
     font-size: 24px;
   }
+
   h3 {
     font-size: 12px !important;
     margin-bottom: 4px;
   }
+
+  ${({ theme }) => theme.mediaQueries.xs} {
+    padding: 30px 24px;
+    height: 300px;
+  }
+
   ${({ theme }) => theme.mediaQueries.sm} {
-    padding: 68px 40% 48px 24px;
+    padding: 48px 40% 48px 24px;
     border-radius: unset;
-    height: 327px;
+    height: 304px;
+
     &:before {
-      width: 40%;
+      margin-right: 2.5em;
       opacity: 1;
     }
+
     h2 {
       font-size: 32px !important;
     }
@@ -65,6 +141,8 @@ const StyledBannerVoting = styled(Card)`
   padding: 48px 24px;
   position: relative;
   overflow: visible;
+  height: 326px;
+
   &:before {
     content: '';
     width: 70%;
@@ -86,10 +164,17 @@ const StyledBannerVoting = styled(Card)`
     font-size: 12px !important;
     margin-bottom: 4px;
   }
+
+  ${({ theme }) => theme.mediaQueries.xs} {
+    padding: 34px 24px;
+    height: 300px;
+  }
+
   ${({ theme }) => theme.mediaQueries.sm} {
     padding: 68px 40% 48px 24px;
     border-radius: unset;
-    height: 327px;
+    height: 304px;
+
     &:before {
       width: 46%;
       opacity: 1;
@@ -103,18 +188,72 @@ const StyledBannerVoting = styled(Card)`
   }
 `
 
-const StyledBannerFinixToken = styled(Card)`
+const StyledBannerLongTerm = styled(Card)`
   width: 100%;
   background: ${({ theme }) => theme.colors.card};
   padding: 48px 24px;
   position: relative;
   overflow: visible;
+  height: 326px;
 
   &:before {
     content: '';
     width: 70%;
     height: 100%;
-    background: ${({ theme }) => (theme.isDark ? `url(${logoFinixBannerFull})` : `url(${logoFinixBannerFull})`)};
+    background: url(${definixLongTerm});
+    background-size: contain;
+    background-position: right bottom;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    right: 0;
+    opacity: 0.2;
+    border-bottom-right-radius: ${({ theme }) => theme.radii.card};
+  }
+  h2 {
+    font-size: 24px;
+  }
+  h3 {
+    font-size: 12px !important;
+    margin-bottom: 4px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.xs} {
+    padding: 30px 24px;
+    height: 300px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    padding: 68px 40% 48px 24px;
+    border-radius: unset;
+    height: 304px;
+
+    &:before {
+      width: 40%;
+      opacity: 1;
+    }
+    h2 {
+      font-size: 32px !important;
+    }
+    h3 {
+      font-size: 16px !important;
+    }
+  }
+`
+
+const StyledBannerSixProtocol = styled(Card)`
+  width: 100%;
+  background: ${({ theme }) => theme.colors.card};
+  padding: 48px 24px;
+  position: relative;
+  overflow: visible;
+  height: 326px;
+
+  &:before {
+    content: '';
+    width: 70%;
+    height: 100%;
+    background: ${({ theme }) => (theme.isDark ? `url(${logoDefinixSixProtocol})` : `url(${logoDefinixSixProtocol})`)};
     background-size: contain;
     background-position: right center;
     background-repeat: no-repeat;
@@ -128,22 +267,21 @@ const StyledBannerFinixToken = styled(Card)`
   h2 {
     font-size: 24px;
   }
+
   h3 {
     font-size: 12px !important;
     margin-bottom: 4px;
   }
 
-  ${({ theme }) => theme.mediaQueries.md} {
-    padding: 90px 40% 48px 40px !important;
-    &:before {
-      margin-right: 40px !important;
-    }
+  ${({ theme }) => theme.mediaQueries.xs} {
+    padding: 80px 24px;
+    height: 300px;
   }
 
   ${({ theme }) => theme.mediaQueries.sm} {
-    padding: 90px 40% 48px 24px;
+    padding: 90px 42% 48px 24px;
     border-radius: unset;
-    height: 327px;
+    height: 304px;
 
     &:before {
       width: 40%;
@@ -152,60 +290,17 @@ const StyledBannerFinixToken = styled(Card)`
     }
 
     h2 {
-      font-size: 28px !important;
-    }
-    h3 {
-      font-size: 16px !important;
-    }
-  }
-`
-
-const StyledBannerRebalance = styled(Card)`
-  width: 100%;
-  background: ${({ theme }) => theme.colors.card};
-  padding: 48px 24px;
-  position: relative;
-  overflow: visible;
-
-  &:before {
-    content: '';
-    width: 100%;
-    height: 100%;
-    background: url(${logoRebalance});
-    background-size: contain;
-    background-position: right bottom;
-    background-repeat: no-repeat;
-    position: absolute;
-    top: 0;
-    right: 0;
-    opacity: 0.2;
-    border-bottom-right-radius: ${({ theme }) => theme.radii.card};
-  }
-
-  h2 {
-    font-size: 24px;
-  }
-  h3 {
-    font-size: 12px !important;
-    margin-bottom: 4px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    padding: 48px 40% 48px 24px;
-    border-radius: unset;
-    height: 327px;
-
-    &:before {
-      // width: 40%;
-      margin-right: 2.5em;
-      opacity: 1;
-    }
-
-    h2 {
       font-size: 32px !important;
     }
     h3 {
       font-size: 16px !important;
+    }
+  }
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    padding: 90px 40% 48px 40px !important;
+    &:before {
+      margin-right: 40px !important;
     }
   }
 `
@@ -275,7 +370,6 @@ const SpecialOutline = styled(Button)`
     &:before {
       transform-origin: 0;
       transform: scaleX(1);
-      // background: #d1eeff;
       border-radius: 40px;
     }
     span {
@@ -290,16 +384,6 @@ const SpecialOutline = styled(Button)`
 `
 
 const StyledSlider = styled(Slider)`
-  .slick-prev {
-    width: 4rem;
-    height: 4rem;
-    left: -3rem;
-  }
-  .slick-next {
-    width: 4rem;
-    height: 4rem;
-    right: -3rem;
-  }
   .slick-prev:before,
   .slick-next:before {
     color: #0973b9;
@@ -318,6 +402,30 @@ const StyledSlider = styled(Slider)`
   }
   .slick-list {
     border-radius: 8px;
+  }
+  ${({ theme }) => theme.mediaQueries.xs} {
+    .slick-prev {
+      width: 5rem;
+      height: 4rem;
+      left: -3rem;
+    }
+    .slick-next {
+      width: 6rem;
+      height: 4rem;
+      right: -3rem;
+    }
+  }
+  ${({ theme }) => theme.mediaQueries.md} {
+    .slick-prev {
+      width: 4rem;
+      height: 4rem;
+      left: -3rem;
+    }
+    .slick-next {
+      width: 4rem;
+      height: 4rem;
+      right: -3rem;
+    }
   }
 `
 const CardAutoRebalancing = ({ className = '' }) => {
@@ -343,25 +451,13 @@ const CardAutoRebalancing = ({ className = '' }) => {
           <a href="https://sixnetwork.medium.com/finix-halving-update-en-6d6a8624ae7d" target="_blank" rel="noreferrer">
             <StyledBannerFinixToken className={className}>
               <div className="pos-relative" style={{ zIndex: 1 }}>
-                <Text
-                  color="primary"
-                  fontSize={isMobile ? '32px !important' : '50px !important'}
-                  bold
-                  className="mb-2"
-                  lineHeight="1"
-                >
+                <Heading className="mb-2" color="primary">
                   FINIX Token’s
-                </Text>
-                <Text
-                  color="text"
-                  fontSize={isMobile ? '28px !important' : '42px !important'}
-                  bold
-                  lineHeight="1"
-                  className="mb-2"
-                >
+                </Heading>
+                <Text color="text" fontSize="22px !important" bold lineHeight="1" className="mb-2">
                   2nd Halving
                 </Text>
-                <Text color="textSubtle" fontSize="16px">
+                <Text color="textSubtle" fontSize="12px !important">
                   21 June 2022 at 04.00 P.M. (GMT +7)
                 </Text>
               </div>
@@ -427,6 +523,29 @@ const CardAutoRebalancing = ({ className = '' }) => {
               </div>
             </div>
           </StyledBannerLongTerm>
+        </div>
+        <div>
+          <a href="https://sixprotocol.com/" target="_blank" rel="noreferrer">
+            <StyledBannerSixProtocol className={className}>
+              <div className="pos-relative" style={{ zIndex: 1 }}>
+                <Heading className="mb-2" color="primary">
+                  BE PREPARE FOR THE USE OF DEFINIX
+                </Heading>
+                <Text
+                  color="text"
+                  fontSize={isMobile ? '16px !important' : '22px !important'}
+                  bold
+                  lineHeight="1"
+                  className="mb-2"
+                >
+                  On SIX Protocol
+                </Text>
+                <Text color="textSubtle" fontSize="14px">
+                  (Update for Definix community is coming soon)
+                </Text>
+              </div>
+            </StyledBannerSixProtocol>
+          </a>
         </div>
       </StyledSlider>
     </div>
